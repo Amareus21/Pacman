117c7e571d89684925fe61098f883770adabc822
miguel <miguelortiz8105@gmail.com>
2022-12-07 21:23:23 +0100

    Colision Obstaculo Hecho

--- ts/pacman.ts
+++ ts/pacman.ts
@@ -3,25 +3,56 @@ function start() {
   let ctx = canvas.getContext("2d");
 
   if (ctx) {
-    const drawPacman = new DrawPacman(ctx, canvas.width, canvas.height);
-    const pacman = new Pacman({ x: 60, y: 60 }, "#FFFF00", drawPacman);
+    const pacman = new Pacman({ x: 65, y: 300 }, 30, "#FFFF00", "right");
+    const drawPacman = new DrawPacman(pacman, ctx, canvas.width, canvas.height);
+    const drawLayer = new DrawLayer(ctx);
     function move(event: KeyboardEvent) {
-      pacman.movePlayer(event);
+      let lastposition = { ...pacman.position };
+      if (event.key === pacman.right) {
+        pacman.position.x += 5;
+        pacman.direction = "right";
+      }
+      if (event.key === pacman.left) {
+        pacman.position.x -= 5;
+        pacman.direction = "left";
+      }
+      if (event.key === pacman.up) {
+        pacman.position.y -= 5;
+        pacman.direction = "up";
+      }
+      if (event.key === pacman.down) {
+        pacman.position.y += 5;
+        pacman.direction = "down";
+      }
+      if (Colision.colision(DrawLayer.arrayobstacles, pacman)) {
+        pacman.position = lastposition;
+      }
     }
+
     window.addEventListener("keypress", move);
+    window.requestAnimationFrame(drawGame);
+    function drawGame() {
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawPacman.draw();
+
+        drawLayer.init();
+        window.requestAnimationFrame(drawGame);
+      }
+    }
   }
 }
 
 class Player {
   _position: position;
   _color: string;
-  draw: DrawPlayer;
-  constructor(p: position, color: string, drawPlayer: DrawPlayer) {
+  _width: number;
+  _direction: string;
+  constructor(p: position, width: number, color: string, direction: string) {
     this._position = p;
     this._color = color;
-    this.draw = drawPlayer;
-    this.draw.position = this._position;
-    this.draw._color = this._color;
+    this._direction = direction;
+    this._width = width;
   }
 
   get position(): position {
@@ -30,15 +61,35 @@ class Player {
   set position(position: position) {
     this._position = position;
   }
+  set positionX(x: number) {
+    this._position.x = x;
+  }
+  set positionY(y: number) {
+    this._position.y = y;
+  }
+  get positionX() {
+    return this._position.x;
+  }
+  get positionY() {
+    return this._position.y;
+  }
   get color(): string {
     return this._color;
   }
   set color(color: string) {
     this._color = color;
   }
-
-  drawPlayer(direction: string) {
-    this.draw.draw(direction);
+  get direction(): string {
+    return this._direction;
+  }
+  set direction(direction: string) {
+    this._direction = direction;
+  }
+  get width(): number {
+    return this._width;
+  }
+  set width(width: number) {
+    this._width = width;
   }
 }
 
@@ -47,30 +98,6 @@ class Pacman extends Player {
   readonly left = "s";
   readonly up = "e";
   readonly down = "d";
-
-  movePlayer(event: KeyboardEvent) {
-    (this.draw as DrawPacman).clean();
-    if (event.key === this.right) {
-      this._position.x += 5;
-      this.drawPlayer(this.right);
-    }
-    if (event.key === this.left) {
-      this._position.x -= 5;
-      this.drawPlayer(this.left);
-    }
-    if (event.key === this.up) {
-      this._position.y -= 5;
-      this.drawPlayer(this.up);
-    }
-    if (event.key === this.down) {
-      this._position.y += 5;
-      this.drawPlayer(this.down);
-    }
-  }
-}
-
-interface Draw {
-  draw(direction: string): void;
 }
 
 type position = {
@@ -78,73 +105,270 @@ type position = {
   y: number;
 };
 
-abstract class DrawPlayer implements Draw {
+abstract class DrawPlayer {
   ctx: CanvasRenderingContext2D;
-  _position: position = { x: 0, y: 0 };
   _color: string = "";
   width: number;
   height: number;
+  player: Player;
 
-  constructor(ctx: CanvasRenderingContext2D, width: number, height: number) {
+  constructor(
+    player: Player,
+    ctx: CanvasRenderingContext2D,
+    width: number,
+    height: number
+  ) {
     this.ctx = ctx;
     this.width = width;
     this.height = height;
+    this.player = player;
   }
-  set position(position: position) {
-    this._position = position;
-  }
-  set color(color: string) {
-    this._color = color;
-  }
-  draw(direction: string): void {}
+
+  abstract draw(): void;
 }
 
 class DrawPacman extends DrawPlayer {
-  override draw(direction: string) {
+  override draw() {
     this.ctx.beginPath();
     this.ctx.fillStyle = "#FFFF00";
-    if (direction === "f") {
+    if (this.player.direction === "right") {
       this.ctx.arc(
-        this._position.x,
-        this._position.y,
-        30,
+        this.player.position.x,
+        this.player.position.y,
+        this.player.width,
         (1 / 4) * Math.PI,
         (7 / 4) * Math.PI
       );
     }
-    if (direction === "s") {
+    if (this.player.direction === "left") {
       this.ctx.arc(
-        this._position.x,
-        this._position.y,
-        30,
+        this.player.position.x,
+        this.player.position.y,
+        this.player.width,
         (5 / 4) * Math.PI,
         (3 / 4) * Math.PI
       );
     }
-    if (direction === "e") {
+    if (this.player.direction === "up") {
       this.ctx.arc(
-        this._position.x,
-        this._position.y,
-        30,
+        this.player.position.x,
+        this.player.position.y,
+        this.player.width,
         (7 / 4) * Math.PI,
         (5 / 4) * Math.PI
       );
     }
-    if (direction === "d") {
+    if (this.player.direction === "down") {
       this.ctx.arc(
-        this._position.x,
-        this._position.y,
-        30,
+        this.player.position.x,
+        this.player.position.y,
+        this.player.width,
         (3 / 4) * Math.PI,
         (1 / 4) * Math.PI
       );
     }
-    this.ctx.lineTo(this._position.x, this._position.y);
+    this.ctx.lineTo(this.player.position.x, this.player.position.y);
     this.ctx.closePath();
     this.ctx.fill();
     this.ctx.stroke();
   }
-  clean() {
-    this.ctx.clearRect(0, 0, this.width, this.height);
+}
+
+abstract class DrawObstacle {
+  width: number;
+  heigth: number;
+  round: number;
+  ctx: CanvasRenderingContext2D;
+  position: position;
+  constructor(
+    ctx: CanvasRenderingContext2D,
+    position: position,
+    width: number,
+    heigth: number,
+    round: number
+  ) {
+    this.ctx = ctx;
+    this.width = width;
+    this.heigth = heigth;
+    this.position = position;
+    this.round = round;
+  }
+  abstract drawObstacle(): void;
+}
+
+class DrawBorder extends DrawObstacle {
+  _isBorder: boolean;
+  constructor(
+    ctx: CanvasRenderingContext2D,
+    position: position,
+    width: number,
+    heigth: number,
+    round: number,
+    isBorder: boolean
+  ) {
+    super(ctx, position, width, heigth, round);
+    this._isBorder = isBorder;
+  }
+  get isBorder(): boolean {
+    return this._isBorder;
+  }
+  drawObstacle() {
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = "#0000FF";
+    this.ctx.roundRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.heigth,
+      this.round
+    );
+    this.ctx.stroke();
+  }
+}
+class DrawLayer {
+  border: DrawObstacle;
+  border2: DrawObstacle;
+  obstacle1: DrawObstacle;
+  obstacle2: DrawObstacle;
+  obstacle3: DrawObstacle;
+  width: number;
+  heigth: number;
+  x: number;
+  y: number;
+  static _arrayObstacles = new Array<DrawObstacle>();
+  static get arrayobstacles() {
+    return this._arrayObstacles;
+  }
+  ctx: CanvasRenderingContext2D;
+  constructor(ctx: CanvasRenderingContext2D) {
+    this.ctx = ctx;
+    this.width = 1230;
+    this.heigth = 735;
+    this.x = 35;
+    this.y = 35;
+    this.border = new DrawBorder(this.ctx, { x: 5, y: 5 }, 1290, 790, 20, true);
+    this.border2 = new DrawBorder(
+      this.ctx,
+      { x: this.x, y: this.y },
+      this.width,
+      this.heigth,
+      20,
+      true
+    );
+    this.obstacle1 = new DrawBorder(
+      this.ctx,
+      { x: this.x + 65, y: this.y + 65 },
+      this.width / 2 - 95,
+      30,
+      20,
+      false
+    );
+    this.obstacle2 = new DrawBorder(
+      this.ctx,
+      { x: this.width / 2 + 65, y: this.y + 65 },
+      this.width / 2 - 95,
+      30,
+      20,
+      false
+    );
+    this.obstacle3 = new DrawBorder(
+      this.ctx,
+      { x: this.x, y: this.y + 155 },
+      300,
+      30,
+      20,
+      false
+    );
+    DrawLayer._arrayObstacles.push(this.obstacle1);
+    DrawLayer._arrayObstacles.push(this.obstacle2);
+    DrawLayer._arrayObstacles.push(this.obstacle3);
+    DrawLayer._arrayObstacles.push(this.border);
+    DrawLayer._arrayObstacles.push(this.border2);
+  }
+  init() {
+    this.border.drawObstacle();
+    this.border2.drawObstacle();
+    for (const obstacle of DrawLayer._arrayObstacles) {
+      obstacle.drawObstacle();
+    }
+  }
+}
+class Colision {
+  static colision(obstacles: Array<DrawObstacle>, player: Player): boolean {
+    for (let obstacle of obstacles) {
+      /*if (
+        player.position.x + player._width > obstacle.position.x &&
+        player.position.x + player.width <
+          obstacle.position.x + obstacle.width &&
+        player.position.y + player.width > obstacle.position.y &&
+        player.position.y + player.width < obstacle.heigth + obstacle.position.y
+      ) {
+        return true;
+      }*/
+      let pointA = {
+        x: player.positionX - player.width,
+        y: player.positionY - player.width,
+      };
+      let pointB = {
+        x: player.positionX + player.width,
+        y: player.positionY - player.width,
+      };
+      let pointC = {
+        x: player.positionX + player.width,
+        y: player.positionY + player.width,
+      };
+      let pointD = {
+        x: player.positionX - player.width,
+        y: player.positionY + player.width,
+      };
+      let a = { ...obstacle.position };
+      let b = {
+        x: obstacle.position.x + obstacle.width,
+        y: obstacle.position.y,
+      };
+      let c = {
+        x: obstacle.position.x + obstacle.width,
+        y: obstacle.position.y + obstacle.heigth,
+      };
+      let d = {
+        x: obstacle.position.x,
+        y: obstacle.position.y + obstacle.heigth,
+      };
+      let arrayPlayer = [pointA, pointB, pointC, pointD];
+      let arrayobstacles = [a, b, c, d];
+      for (const point of arrayPlayer) {
+        let sign1 = Math.sign(calProdVectorial(point, a, b));
+        let sign2 = Math.sign(calProdVectorial(point, b, c));
+        let sign3 = Math.sign(calProdVectorial(point, c, d));
+        let sign4 = Math.sign(calProdVectorial(point, d, a));
+        if ((<DrawBorder>obstacle).isBorder) {
+          if (sign1 !== sign2 || sign3 !== sign4 || sign1 !== sign3) {
+            return true;
+          }
+        } else if (sign1 === sign2 && sign3 === sign4 && sign1 === sign3) {
+          return true;
+        }
+      }
+      for (const point of arrayobstacles) {
+        let sign1 = Math.sign(calProdVectorial(point, pointA, pointB));
+        let sign2 = Math.sign(calProdVectorial(point, pointB, pointC));
+        let sign3 = Math.sign(calProdVectorial(point, pointC, pointD));
+        let sign4 = Math.sign(calProdVectorial(point, pointD, pointA));
+        if (sign1 === sign2 && sign3 === sign4 && sign1 === sign3) {
+          return true;
+        }
+      }
+      function calProdVectorial(
+        point: position,
+        a: position,
+        b: position
+      ): number {
+        let v = { x: a.x - point.x, y: a.y - point.y };
+        let w = { x: b.x - point.x, y: b.y - point.y };
+        let vw = v.x * w.y - v.y * w.x;
+        return vw;
+      }
+    }
+    return false;
   }
 }
